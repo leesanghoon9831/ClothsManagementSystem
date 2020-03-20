46989d9ba7205558236ff32cc3adb26ecd83f343
LG <LG@DESKTOP-TITG8O4>
2020-03-20 22:35:07 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		
+		System.out.println("<Welcome to Closet!>");
+		System.out.println("1. Add Cloths");
+		System.out.println("2. Delete Cloths");
+		System.out.println("3. Edit Cloths");
+		System.out.println("4. View Cloths");
+		System.out.println("5. Exit");
+		
+	}
+}
