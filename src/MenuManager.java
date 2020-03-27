4699096d608b334c8261562cd8395c7cb4f67ac6
LG <LG@DESKTOP-TITG8O4>
2020-03-27 20:36:00 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,13 +1,63 @@
+import java.util.Scanner;
 
-public class HelloWorld {
+public class MenuManager {
 	public static void main(String[] args) {
-		
-		System.out.println("<Welcome to Closet!>");
-		System.out.println("1. Add Cloths");
-		System.out.println("2. Delete Cloths");
-		System.out.println("3. Edit Cloths");
-		System.out.println("4. View Cloths");
-		System.out.println("5. Exit");
-		
+		Scanner input = new Scanner(System.in);
+		int num = 5;
+
+		while (num != 6) {
+			System.out.println("<Welcome to Closet!>");
+			System.out.println("1. Add Cloths");
+			System.out.println("2. Delete Cloths");
+			System.out.println("3. Edit Cloths");
+			System.out.println("4. View Cloths");
+			System.out.println("5. Show a menu");
+			System.out.println("6. Exit");
+			System.out.print("Select one : ");
+			num = input.nextInt();
+			if (num == 1) {
+				addCloths();
+			}
+			else if (num == 2) {
+				deleteCloths();
+			}
+			else if (num == 3) {
+				editCloths();
+			}
+			else if (num == 4) {
+				viewCloths();
+			}
+			else {
+				continue;
+			}
+		}
 	}
+	public static void addCloths() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Cloths ID : ");
+		int clothsId = input.nextInt();
+		String enter = input.nextLine();
+		System.out.print("Cloths name : ");
+		String clothsName = input.nextLine();
+		System.out.print("Cloths kind : ");
+		String clothsKind = input.next();
+		System.out.print("Cloths price : ");
+		int clothsPrice = input.nextInt();
+	}
+	public static void deleteCloths() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Cloths ID : ");
+		int clothsId = input.nextInt();
+	}
+	public static void editCloths() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Cloths ID : ");
+		int clothsId = input.nextInt();
+	}
+	public static void viewCloths() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Cloths ID : ");
+		int clothsId = input.nextInt();
+	}
+
 }
